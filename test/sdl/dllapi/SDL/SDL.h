cc47b1cecc117d4d55ec8577bb9894d41ca317ee
wang-bin <wbsecg1@gmail.com>
2014-04-15 14:51:17 +0800

    tests: remove sdl headers. now we can simply include SDL.h

--- test/sdl/dllapi/SDL/SDL.h
+++ test/sdl/dllapi/SDL/SDL.h
@@ -4,12 +4,12 @@
 namespace dllapi {
 namespace SDL {
 
-//TODO why include SDL/SDL.h will cause link error? otherwise, the other line are not required
-//#include "SDL/SDL.h"
+// you can simply include "SDL/SDL.h" here and without any other code.
+#include "SDL/SDL.h"
 /*
- *if include SDL/SDL.h is ok, the code below is not requred
- *copy from SDL headers without any change
+ *copy from SDL headers without any change.
  */
+/*
 #include "SDL/SDL_stdinc.h"
 #include "SDL/SDL_events.h"
 #include "SDL/SDL_video.h"
@@ -21,6 +21,7 @@ extern DECLSPEC int SDLCALL SDL_PollEvent(SDL_Event *event);
 extern DECLSPEC SDL_Surface * SDLCALL SDL_SetVideoMode
             (int width, int height, int bpp, Uint32 flags);
 extern DECLSPEC void SDLCALL SDL_WM_SetCaption(const char *title, const char *icon);
+*/
 } //namespace SDL
 } //namespace dllapi
 
